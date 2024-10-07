d25c5f122a09e6534f6b6e81988f05e118d85365
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-07 04:03:28 +0000

    datatypes

---
+++ project5.js/datat.js
@@ -0,0 +1,42 @@
+let state = "null"
+console.log ("state");
+console.log(typeof null);
+let score=33
+console.log(typeof score);
+         // operations  
+let value=3
+let negvalue = value
+console.log(negvalue);
+console.log(2-2);
+console.log(2%2);
+let str1="aadi"
+let str2 = " pradhan"
+let str3= str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+1);
+console.log("1"+"2");
+let counter=33
+counter++
+console.log(counter);
+console.log("2">1);
+console.log(2>1);
+
+const favNumber = 33
+const name = "aadi"
+let roomTemp= null
+let online = false
+let productivit;
+console.log(productivit);
+console.log(typeof name);
+
+
+    const myfunction=function(){
+        console.log("yooo");
+        
+
+    }
+
+    
+
+
