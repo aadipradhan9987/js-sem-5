7906a60cff58208e5cef5c7ced7e2b738adff91c
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-09 08:33:27 +0000

    num and maths

---
+++ project5.js/num.js
@@ -0,0 +1,45 @@
+const score= 25
+console.log(score);
+
+// these are use speciall for E-Commers website
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNum = 21.556
+console.log(otherNum.toPrecision(3));
+
+const hundred = 10000000
+console.log(hundred.toLocaleString('en-IN'));
+ 
+//++++++++  MATHS 
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log((Math.round(3.8)));
+console.log(Math.floor(3.4));
+console.log(Math.max(2,4,5,8));
+
+// used while maing the dice game
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const max = 100
+const min = 10
+
+console.log(Math.floor(Math.random()*(max-min +1))+ min);
+
+
+
+
+
+
+
+
+
+
+
