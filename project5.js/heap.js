9ba9f87b0393ebbce89b8fd8f27691600965c06a
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-09 07:34:48 +0000

    3rd

---
+++ project5.js/heap.js
@@ -0,0 +1,44 @@
+let myName = "aaditapradhandot"
+let anotherName = myName
+anotherName = "ahsirk"
+console.log(myName);
+console.log(anotherName);
+
+
+// string 
+
+let name = "aadi"
+let repoCount = 23
+console.log(`hello m name is ${name } and m repocount is ${repoCount}`);
+
+// new isthe object of js
+const gameName = new String('aadi')
+//console.log(gameName[d]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("i"));
+console.log(gameName.substring(0,2));
+
+const anotherString = gameName.slice(-4,2);
+console.log(anotherString);
+
+const newString = "   pradhan    "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "https://servere%20pradhann"
+console.log(url.replace('%20','-'));
+console.log (url.includes('servere'))
+
+
+
+
+
+
+
+
+ 
+
+
