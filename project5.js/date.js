4adb60f0c2b6ae8fc83a532972762b338dd4a2ba
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-11 12:29:04 +0000

    dare

---
+++ project5.js/date.js
@@ -0,0 +1,19 @@
+
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+//console.log(myDate.toLocalString());
+console.log(typeof myDate);
+
+ //let myCreatedDate = new Date (2025 ,0 23)
+//console.log(myCreatedDate.toDateString());
+
+//let myDob = new Date(2002 ,1 , 4 , 2 ,3)
+///console.log(myDob.toLocaleDateString);
+
+let myCreatedDate = new Date ("02-04-2002")
+console.log(myCreatedDate.toLocaleDateString);
+
+
+
