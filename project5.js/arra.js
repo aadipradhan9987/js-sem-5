16b94abba8a996e51f64398a7daacce7390734c4
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-12 13:47:52 +0000

    ara1

---
+++ project5.js/arra.js
@@ -0,0 +1,26 @@
+//arrays
+
+const myArr = [0 ,1 ,2 ,3 ,4 , 5]
+const myHero = [ "batman" , "superman" ]
+console.log(myArr[2]);
+
+//array methods
+
+//myArr.push(6);
+//myArr.pop();
+
+//myArr.unshift(4);
+//myArr.shift(6)
+
+
+//console.log(myArr.includes(4));
+//console.log(myArr.indexOf(3));
+
+
+
+
+
+console.log(myArr);
+
+
+
