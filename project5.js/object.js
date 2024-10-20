48671dcb199e680ed1327b7a31b60544a472921c
aadipradhan9987 <pradhanaditya9987@gmail.com>
2024-10-20 16:26:20 +0000

    dfdf

---
+++ project5.js/object.js
@@ -0,0 +1,33 @@
+//object literals
+
+//const mySym = Symbol("key1")
+
+//const Jsuser ={
+    //name :"aaditya",
+   // mySym: "mykey1",
+   // location:"biratnagar",
+   // Isloggedin : ["monday"],
+   // age : 21,
+}
+//console.log(Jsuser.age);
+//console.log(Jsuser.location); 
+//console.log(Jsuser.name);
+//console.log(Jsuser[mySym]);
+
+//Jsuser.location ="kathmandu"
+//Object.freeze(Jsuser)
+Jsuser.location ="chitwanu"
+
+Jsuser.greeting = function(){
+    console.log("Hello User");
+    
+}
+
+console.log(Jsuser.greeting);
+
+
+
+
+
+
+
